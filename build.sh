e51d96c399f4a06fd7f2eaa81f9a9444f0b6f77e
Vedant Desai <vedant.code@gmail.com>
2021-09-22 19:41:41 +0530

    Added RO object and shell scripts

--- build.sh
+++ build.sh
@@ -1,21 +1,33 @@
-echo "------------------------ Started ------------------------";
-echo "Need to : $1";
+echo "------------------------ Started [$1] ------------------------";
 
 if [ "$1" == "image" ]
 then
-   docker build -t  .
+   docker build -f "Dockerfile" --tag k8s-spring-boot-application "."
+   docker tag k8s-spring-boot-application:latest vedantkdesai/k8s-spring-boot-application:v1.0.0
+fi
+
+if [ "$1" == "image-push" ]
+then
+   docker push vedantkdesai/k8s-spring-boot-application:tagname
 fi
 
 if [ "$1" == "stop" ]
 then
    docker-compose -f "docker-compose.yml" down
+   kubectl delete -f deployment.yaml
 fi
 
 if [ "$1" == "deploy" ]
 then
    mvn clean install 
-   docker build --pull --rm -f "Dockerfile" -t k8sapplication:v1 "."
+   docker build -f "Dockerfile" --tag k8s-spring-boot-application "."
+   docker tag k8s-spring-boot-application:latest vedantkdesai/k8s-spring-boot-application:v1.0.0
    docker-compose -f "docker-compose.yml" down
    docker-compose -f "docker-compose.yml" up -d --build
 fi
-echo "------------------------ Ended   ------------------------";
+
+if [ "$1" == "k-deploy" ]
+then
+   kubectl apply -f deployment.yaml
+fi
+echo "------------------------ Ended   [$1] ------------------------";
--- src/main/java/com/k8s/example/k8sapplication/ServerRO.java
+++ src/main/java/com/k8s/example/k8sapplication/ServerRO.java
@@ -3,30 +3,37 @@ package com.k8s.example.k8sapplication;
 import java.util.Date;
 
 public class ServerRO {
-    private Date serverDate;
-    private Date refreshDate;
+    private Date serverStartDate;
+    private Date refreshNowDate;
 
     public ServerRO(){
 
     }
 
-    public ServerRO(Date severStartDate, Date refreshDate) {
-        this.serverDate = severStartDate;
-        this.refreshDate = refreshDate;
+    public ServerRO(Date severStartDate, Date refreshNowDate) {
+        this.serverStartDate = severStartDate;
+        this.refreshNowDate = refreshNowDate;
     }
 
-    public Date getServerDate() {
-        return serverDate;
-    }
-    public void setServerDate(Date serverDate) {
-        this.serverDate = serverDate;
-    }
-    public Date getRefreshDate() {
-        return refreshDate;
-    }
-    public void setRefreshDate(Date refreshDate) {
-        this.refreshDate = refreshDate;
+    public Date getServerStartDate() {
+        return serverStartDate;
     }
 
+    public void setServerStartDate(Date serverStartDate) {
+        this.serverStartDate = serverStartDate;
+    }
+
+    public Date getRefreshNowDate() {
+        return refreshNowDate;
+    }
+
+    public void setRefreshNowDate(Date refreshNowDate) {
+        this.refreshNowDate = refreshNowDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerRO [refreshNowDate=" + refreshNowDate + ", serverStartDate=" + serverStartDate + "]";
+    }
     
 }
